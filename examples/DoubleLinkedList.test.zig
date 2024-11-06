abf7bbdd442a880b9ef4e54a73354cad560eff22
Luis Rodríguez <luis.rodriguez@leanmind.es>
2024-11-06 18:35:40 +0100

    update expected

--- examples/DoubleLinkedList.test.zig
+++ examples/DoubleLinkedList.test.zig
@@ -2,10 +2,10 @@ const std = @import("std");
 const testing = @import("std").testing;
 const DoubleLinkedList = @import("DoubleLinkedList.zig");
 
-test "when Double linked list is created, it is empty and its length is zero" {
+test "When Double linked list is created, it is empty and its length is zero" {
     var list = DoubleLinkedList.create(u32).init(testing.allocator);
     defer list.deinit();
 
-    try testing.expectEqual(0, list.len());
+    try testing.expectEqual(@as(usize, 0), list.len());
     try testing.expect(list.isEmpty());
 }
