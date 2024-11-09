51803efa925023c0ce69d919ce96f86b872fb925
Luis Rodríguez <seymourpoler@gmail.com>
2024-11-09 16:48:22 +0100

    starting point for developing a Http Server

--- examples/HttpServer.zig
+++ examples/HttpServer.zig
@@ -32,38 +32,6 @@ fn handleConnection(allocator: *std.mem.Allocator, server_socket: std.net.Stream
 
     std.log.info("Received request: {}", .{request});
 
-    const response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Hello, Zig!</h1>";
-    try writer.writeAll(response);
-
-    server_socket.close() catch |err| {
-        std.log.err("Failed to close connection: {}", .{err});
-    };
-}
-
-fn sendFile(writer: std.io.Writer, filename: []const u8) !void {
-    const file = try std.fs.cwd().openFile(filename, .{});
-    defer file.close();
-
-    var buffer: [1024]u8 = undefined;
-    while (true) {
-        const read_bytes = try file.read(buffer[0..]);
-        if (read_bytes.len == 0) break;
-        try writer.writeAll(read_bytes);
-    }
-}
-
-
-fn handleConnection(allocator: *std.mem.Allocator, server_socket: std.net.StreamServerSocket) !void {
-    const reader = server_socket.reader();
-    const writer = server_socket.writer();
-
-    var buffer: [1024]u8 = undefined;
-
-    const size = try reader.readAll(buffer[0..]);
-    const request = buffer[0..size];
-    
-    std.log.info("Received request: {}", .{request});
-
     if (std.mem.startsWith(u8, request, "GET / ")) {
         const response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
         try writer.writeAll(response);
@@ -81,25 +49,14 @@ fn handleConnection(allocator: *std.mem.Allocator, server_socket: std.net.Stream
     };
 }
 
+fn sendFile(writer: std.io.Writer, filename: []const u8) !void {
+    const file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
 
-pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-
-    var listener = try std.net.tcpListen(std.AddressFamily.ipv4);
-    defer listener.deinit();
-
-    try listener.bind(.{
-        .address = std.net.Address.ipv4(.{ .addr = 0, .port = 8080 }),
-        .reuse_address = true,
-    });
-
-    const acceptor = async acceptConnections(allocator, listener);
-    try acceptor.await;
-}
-
-fn acceptConnections(allocator: *std.mem.Allocator, listener: std.net.StreamServerSocket) !void {
+    var buffer: [1024]u8 = undefined;
     while (true) {
-        var server_socket = try listener.accept();
-        _ = async handleConnection(allocator, server_socket);
+        const read_bytes = try file.read(buffer[0..]);
+        if (read_bytes.len == 0) break;
+        try writer.writeAll(read_bytes);
     }
 }
